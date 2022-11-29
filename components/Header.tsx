e44e6f3fb92d7c419180d1cd212276f6794b39ce
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-11-29 10:27:01 +0700

    add `Header`

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import {SocialIcon} from "react-social-icons";
+import {motion} from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <div>Header</div>
-  )
+    <header className="sticky top-0 p-5 flex items-center justify-between max-w-7xl mx-auto z-20">
+      <motion.div
+        initial={{x: -500, opacity: 0, scale: .5}}
+        animate={{x: 0, opacity: 1, scale: 1}}
+        transition={{duration: 1.5}}
+        className="flex flex-row items-center"
+      >
+        {/* Social icons */}
+        <SocialIcon
+          url="https://www.youtube.com/@masadamsahid"
+          fgColor='gray'
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.youtube.com/@masadamsahid"
+          fgColor='gray'
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.youtube.com/@masadamsahid"
+          fgColor='gray'
+          bgColor="transparent"
+        />
+      </motion.div>
+      
+      <motion.div
+        initial={{x: 500, opacity: 0, scale: .5}}
+        animate={{x: 0, opacity: 1, scale: 1}}
+        transition={{duration: 1.5}}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          className='cursor-pointer'
+          network='email'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <p className="uppercase sm:block hidden text-gray-400">
+          Get in Touch
+        </p>
+      </motion.div>
+    </header>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       </Head>
       
       {/* HEADER */}
+      <Header/>
       
       {/* HERO */}
       
