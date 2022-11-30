75a3729de03a66af73339222a1d8405c4aacf08b
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-11-30 14:42:28 +0700

    add home button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {SocialIcon} from "react-social-icons";
 import {motion} from "framer-motion";
+import Link from "next/link";
 
 type Props = {};
 
@@ -31,22 +32,24 @@ export default function Header({}: Props) {
         />
       </motion.div>
       
-      <motion.div
-        initial={{x: 500, opacity: 0, scale: .5}}
-        animate={{x: 0, opacity: 1, scale: 1}}
-        transition={{duration: 1.5}}
-        className="flex flex-row items-center text-gray-300 cursor-pointer"
-      >
-        <SocialIcon
-          className='cursor-pointer'
-          network='email'
-          fgColor='gray'
-          bgColor='transparent'
-        />
-        <p className="uppercase sm:block hidden text-gray-400">
-          Get in Touch
-        </p>
-      </motion.div>
+      <Link href='#contact-me'>
+        <motion.div
+          initial={{x: 500, opacity: 0, scale: .5}}
+          animate={{x: 0, opacity: 1, scale: 1}}
+          transition={{duration: 1.5}}
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+        >
+          <SocialIcon
+            className='cursor-pointer'
+            network='email'
+            fgColor='gray'
+            bgColor='transparent'
+          />
+          <p className="uppercase sm:block hidden text-gray-400">
+            Get in Touch
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,12 @@ import Experience from "../components/Experience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
+import Link from "next/link";
+import {HomeModernIcon} from "@heroicons/react/24/solid";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-amber-400/80">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-amber-400/80 scroll-smooth">
       <Head>
         <title>Masadamsahid's Portfolio</title>
       </Head>
@@ -41,6 +43,13 @@ const Home: NextPage = () => {
         <ContactMe/>
       </section>
       
+      <Link href='#hero'>
+        <footer className="sticky bottom-5 w-full cursor-pointer flex items-center justify-center">
+          <div className="p-1 bg-amber-300 h-10 w-10 rounded-full grayscale hover:grayscale-0 flex justify-center items-center text-gray-500 hover:text-black">
+            <HomeModernIcon className="h-5 w-5"/>
+          </div>
+        </footer>
+      </Link>
     </div>
   )
 }
